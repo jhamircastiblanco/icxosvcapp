6743ed598dd2592722ce2d919d159d450b0a945b
Jhamir Castiblanco <jhamir.castiblanco@imaginecx.co>
2020-05-02 15:07:20 -0500

    funciona

--- App.js
+++ App.js
@@ -76,54 +76,30 @@ const App = () => {
         }
     }
 
-    return ( <
-            View style = { styles.container } >
-            <
-            TextInput onChangeText = { val => setInput('name', val) }
-            style = { styles.input }
-            value = { formState.name }
-            placeholder = "Nombre" /
-            >
-            <
-            TextInput onChangeText = { val => setInput('description', val) }
-            style = { styles.input }
-            value = { formState.description }
-            placeholder = "Descripción" /
-            >
-            <Header title="Welcome to React Native"/>
-            <Text style={heading}>Step One</Text>
-            <Text>
-            Edit App.js to change this screen and turn it
-            into your app.
-            </Text>
-            <Text style={heading}>See Your Changes</Text>
-            <Text>
-            Press Cmd + R inside the simulator to reload
-            your app’s code.
-            </Text>
-            <Text style={heading}>Debug</Text>
-            <Text>
-            Press Cmd + M or Shake your device to open the
-            React Native Debug Menu.
-            </Text>
-            <Text style={heading}>Learn</Text>
-            <Text>
-            Read the docs to discover what to do next:
-            </Text>
-            <
-            Button title = "Create OSvCApp"
-            onPress = { addOSvCApp }
-            /> {
-            OSvCApps.map((OSvCApp, index) => ( <
-                View key = { OSvCApp.id ? OSvCApp.id : index }
-                style = { styles.OSvCApp } >
-                <
-                Text style = { styles.OSvCAppName } > { OSvCApp.name } < /Text> <
-                Text > { OSvCApp.description } < /Text> < /
-                View >
+    return (
+        <View style={styles.container}>
+        <TextInput
+            onChangeText={val => setInput('name', val)}
+            style={styles.input}
+            value={formState.name} 
+            placeholder="Name"
+        />
+        <TextInput
+            onChangeText={val => setInput('description', val)}
+            style={styles.input}
+            value={formState.description}
+            placeholder="Description"
+        />
+        <Button title="Create Todo" onPress={addTodo} />
+        {
+            todos.map((OSvCApp, index) => (
+            <View key={OSvCApp.id ? OSvCApp.id : index} style={styles.OSvCApp}>
+                <Text style={styles.todoName}>{OSvCApp.name}</Text>
+                <Text>{OSvCApp.description}</Text>
+            </View>
             ))
-        } <
-        /View>
+        }
+        </View>
 )
 }
 
