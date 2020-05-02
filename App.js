90988977621eed68d2691b3bef41de19b90d1e52
Jhamir Castiblanco <jhamir.castiblanco@imaginecx.co>
2020-05-02 14:00:36 -0500

    3rd change

--- App.js
+++ App.js
@@ -90,6 +90,26 @@ const App = () => {
             value = { formState.description }
             placeholder = "Descripción" /
             >
+            <Header title="Welcome to React Native"/>
+            <Text style={heading}>Step One</Text>
+            <Text>
+            Edit App.js to change this screen and turn it
+            into your app.
+            </Text>
+            <Text style={heading}>See Your Changes</Text>
+            <Text>
+            Press Cmd + R inside the simulator to reload
+            your app’s code.
+            </Text>
+            <Text style={heading}>Debug</Text>
+            <Text>
+            Press Cmd + M or Shake your device to open the
+            React Native Debug Menu.
+            </Text>
+            <Text style={heading}>Learn</Text>
+            <Text>
+            Read the docs to discover what to do next:
+            </Text>
             <
             Button title = "Create OSvCApp"
             onPress = { addOSvCApp }
